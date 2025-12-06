b74b5d804f376e51f7ea1c6cc09345f5453d3b8b
GrahamBrown-OregonState <browgrah@oregonstate.edu>
2025-12-06 12:54:38 -0800

    final solution

--- day1/problem1.c
+++ day1/problem1.c
@@ -10,20 +10,23 @@ Author's Note: Yes, this is obviously overkill. I chose to do it this way becaus
 void read_rotations() {
     FILE* rotations_file = fopen("./rotations.txt", "r");
     int current_pos = 50;
-    while (!feof(rotations_file)) {
-        char* line = NULL;
-        size_t buffer_size = 0;
-        ssize_t line_len = getline(&line, &buffer_size, rotations_file);
+    char* line = NULL;
+    size_t buffer_size = 0;
+    ssize_t line_len = 0;
+    int count = 0;
+    while ((line_len = getline(&line, &buffer_size, rotations_file)) != -1) {
         current_pos = calculate_code(line, current_pos);
-        printf("The answer is: %d \n", current_pos);
+        if (current_pos == 0) {
+            count++;
+        }
     }
-    printf("The answer is: %d \n", current_pos);
+    printf("The answer is: %d \n", count);
 
 }
 
 int calculate_code(char* instruction, int current_pos) {
     char num_instr[100];
-    copy_until_null(num_instr, instruction);
+    copy_until_null(num_instr, instruction + 1);
     int num = atoi(num_instr);
     if (instruction[0] == 'L') {
         current_pos -= num;
@@ -35,13 +38,13 @@ int calculate_code(char* instruction, int current_pos) {
         while (current_pos > 99) {
             current_pos -= 100;
         }
-
     }
+    return current_pos;
 }
 
-void copy_until_null(char* dest, const char* src_start) {
+void copy_until_null(char* dest, char* src_start) {
     int i = 0;
-    while (src_start[i] != '\0') {
+    while (src_start[i] != '\0' && src_start[i] != '\n') {
         dest[i] = src_start[i];
         i++;
     }
